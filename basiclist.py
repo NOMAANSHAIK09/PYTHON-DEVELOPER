908ee2f455ddb2749b574e0872148012ec0850d4
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2025-08-26 22:00:30 +0530

    this are basic list exercise from acadamic

---
+++ basiclist.py
@@ -0,0 +1,19 @@
+# to empty the list
+cars=["c1","c2","c3"]
+print(cars)
+cars.clear()
+print(cars)
+
+# To merge the list
+carsA=["c1","c2","c3"]
+carsB=["c4","c5","c6"]
+merged=carsA+carsB
+print(merged)
+
+#to convert another collection to list
+carz={1,2,3,4,}
+print(type(carz))
+change=list(carz)
+print(type(change))
+
+
