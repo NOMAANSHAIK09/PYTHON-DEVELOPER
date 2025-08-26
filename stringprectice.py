47f8ccd594201d03749a3139cd9764cceea6c118
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2025-08-26 21:59:45 +0530

    to simple exercise from apna college course

---
+++ stringprectice.py
@@ -0,0 +1,8 @@
+#wap to input user name lenght 
+name = input("enter the name: ")
+print(name)
+print(len(name))
+
+# wap to find occurence of '$' in astring 
+d=input("doller:")
+print(d.count("$"))
