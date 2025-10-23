b3f9ddab3b329c64440ce4d86b5b85a9edb9ef4b
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2025-10-23 13:01:31 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+#list[]
+print("this is my list")
+
+myList=[1,2,3,4,5,]
+print(myList)
+print(myList[2])
+print(type(myList))
+print(id(myList))
+print("\n")
+
+#tuple()
+print("this is my tuple")
+myTuple=(1,2,3,4,5,)
+print(myTuple)
+print(myTuple[2])
+print(type(myTuple))
+print(id(myTuple))
+print("\n")
+
+
+#dictionary{}
+print("this is my dictonary")
+
+myDist={0:"sunday",1:"monday",2:"tuesday"}
+print(myDist)
+print(myDist[1])
+print(type(myDist))
