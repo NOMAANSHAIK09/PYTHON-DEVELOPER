0b063d97a1f34df26385f68689ea6faf101fad90
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2026-01-18 21:03:47 +0530

    mysql studentmng

---
+++ mysqlstd/sstudents.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE student_db;
+SHOW DATABASES;
+
+USE student_db;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    course VARCHAR(50),
+    mobile numeric(20)
+);
+
+USE student_db;
+
+
+
+
+
+
+
